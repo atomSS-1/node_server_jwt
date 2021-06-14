64de644dead5727c11ae5570d458dbfe55782c39
k89jy <k89jy12@gmail.com>
2021-06-14 12:12:52 +0900

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config({ path: "./env.test" });
-const env = process.env.NODE_ENV;
-const jsFile = require("./test2");
-console.log(jsFile);
-console.log(process.env.NODE_ENV);
-const jsonfile = require("./test2")[env];
-console.log(jsonfile);
